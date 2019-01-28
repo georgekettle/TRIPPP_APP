f7f14b642b5a18510e2bf4d3785886c57824dd97
George Kettle <georgekettle.gk@gmail.com>
2019-01-29 10:03:34 +1100

    React Navba done

--- app/javascript/app/containers/nav_bar.jsx
+++ app/javascript/app/containers/nav_bar.jsx
@@ -10,16 +10,18 @@ class NavBar extends Component {
     return (
       <div className="react-navbar">
         <img src={logo} alt={"logo"} className="navbar-logo"/>
-        <div className="navbar-right">
-          <Link to={'/pins/new'} className="navbar-item navbar-link">
-            <div className="navbar-create-div">
-              <img src={plusIcon} className="plus-icon"/>
-            </div>
-          </Link>
-          <Link to={`/profile/${this.props.currentUser.user_name}`} className="navbar-item navbar-link">
-            <img src={this.props.currentUser.photo} className="avatar"/>
-          </Link>
-        </div>
+        {this.props.currentUser &&
+          <div className="navbar-right">
+            <Link to={'/pins/new'} className="navbar-item navbar-link">
+              <div className="navbar-create-div">
+                <img src={plusIcon} className="plus-icon"/>
+              </div>
+            </Link>
+            <Link to={`/profile/${this.props.currentUser.user_name}`} className="navbar-item navbar-link">
+              <img src={this.props.currentUser.photo} className="avatar"/>
+            </Link>
+          </div>
+        }
       </div>
     );
   }
