6fc01f5f5ec334e952be15300cd241cc1aff5f20
George Kettle <georgekettle.gk@gmail.com>
2019-04-25 18:57:25 +1000

    User Profile trips list and pinslist remount when changing user profiles

--- app/javascript/app/components/profile.jsx
+++ app/javascript/app/components/profile.jsx
@@ -9,20 +9,39 @@ import TripList from '../containers/trip_list';
 import { fetchUser } from '../actions';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // key state used to update the TripList & PinList when params.user_name changes
+      key: 1
+    };
+  }
+
   componentWillMount() {
     this.props.fetchUser(this.props.match.params.user_name);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.user_name !== this.props.match.params.user_name) {
+      //call your api and update state with new props
+      console.log("RENDER NOW");
+      this.props.fetchUser(nextProps.match.params.user_name);
+      // key state used to update the TripList & PinList when params.user_name changes
+      this.setState({ key: Math.random() });
+      window.scrollTo(0, 0);
+    };
   }
 
   renderContent = (tab) => {
     console.log(tab);
     switch(tab) {
       case 'guides': {
-        return <TripList user_name={this.props.match.params.user_name}/>
+        return <TripList key={this.state.key} user_name={this.props.match.params.user_name}/>
       };
       case 'pins': {
-        return <PinList user_name={this.props.match.params.user_name} context="profile"/>
+        return <PinList key={this.state.key} user_name={this.props.match.params.user_name} context="profile"/>
       };
-      default: return <TripList user_name={this.props.match.params.user_name}/>;
+      default: return <TripList key={this.state.key} user_name={this.props.match.params.user_name}/>;
     }
   }
 
